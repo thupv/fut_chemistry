211e656ddfed1066b007428d7bf06ac709db95b0
Thu Pham <pham@pixcap.com>
2022-12-14 02:27:30 +0700

    add icon

--- lib/constants/ad_manager.dart
+++ lib/constants/ad_manager.dart
@@ -3,6 +3,7 @@ import 'dart:io' show Platform;
 class AdManager {
   static final interstitialAds_1 = Platform.isAndroid
       ? testInterstitialAdsId
-      : testInterstitialAdsId;
+      : interstitialAdsIOSId;
   static const testInterstitialAdsId = 'ca-app-pub-3940256099942544/5354046379';
+  static const interstitialAdsIOSId = 'ca-app-pub-6038936385902534/8676361556';
 }
