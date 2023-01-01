e48f4e0aed235e55ae6af2f28bc3c0d92a41283f
Thu Pham <pham@pixcap.com>
2023-01-01 15:47:39 +0700

    - try to fix analytics android

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -25,7 +25,10 @@ class DefaultFirebaseOptions {
       case TargetPlatform.iOS:
         return ios;
       case TargetPlatform.macOS:
-        return macos;
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
       case TargetPlatform.windows:
         throw UnsupportedError(
           'DefaultFirebaseOptions have not been configured for windows - '
@@ -55,7 +58,7 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyCPkWa2eNW1m1whT6O7kOy6AOQ9yhw6u6c',
-    appId: '1:576664634524:android:4764ffc8326e9751419959',
+    appId: '1:576664634524:android:79281af3ea36e64b419959',
     messagingSenderId: '576664634524',
     projectId: 'futchemistryapp',
     storageBucket: 'futchemistryapp.appspot.com',
@@ -69,15 +72,5 @@ class DefaultFirebaseOptions {
     storageBucket: 'futchemistryapp.appspot.com',
     iosClientId: '576664634524-33707rsob3h85iarv7ito3tandufe75m.apps.googleusercontent.com',
     iosBundleId: 'com.futfc.futchemistry',
-  );
-
-  static const FirebaseOptions macos = FirebaseOptions(
-    apiKey: 'AIzaSyANsFsT4v80K2fmNEYsQFKJXY5sZr6LnEU',
-    appId: '1:576664634524:ios:1a4d07a396f7eedf419959',
-    messagingSenderId: '576664634524',
-    projectId: 'futchemistryapp',
-    storageBucket: 'futchemistryapp.appspot.com',
-    iosClientId: '576664634524-l1enejmhtr7esqrde4lp0rdomsjtq8c1.apps.googleusercontent.com',
-    iosBundleId: 'com.thupvvvv.boilerplate.tinyAppBoilerplate',
   );
 }
